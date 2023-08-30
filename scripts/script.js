c84691be5c5c007380998ab2432cbea65fa10f3e
julianabrito0209 <juliana0209@gmail.com>
2023-08-30 15:55:02 -0300

    inserção de pagina com scriptJs

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 //const mobile = documento.getElementById("mobile")
 //const mobile = documento.querySelector("#mobile")
 //javascript (acima)
-const mobile = $("#mobile")
-const desktop = $("#mobile")
-const flowwork = $("#mobile")
-const descricaoMobile = $("#descricaoMobile")
-const descricaoDesktop = $("#descricaoDesktop")
-const descricaoFlowwork = $("#descricaoFlowwork")
+const bolsaPraia = $("#bolsaPraia")
+const clutch = $("#clutch")
+const maeFilha = $("#maeFilha")
+const descricaoBolsaPraia = $("#descricaoMobile")
+const descricaoClutch = $("#descricaoClutch")
+const descricaoMaeFilha = $("#descricaoMaeFilha")
 
 //mobile.addEventListener('click', mostrarDescMobile)
 //function mostrarDescMobile(){
